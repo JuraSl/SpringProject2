dc0455648d032abbaa680ff95eb27242d3dc6694
Slava Juravliov <prospect.sjp@gmail.com>
2019-04-03 14:08:19 +0100

    Arbitrary Maps as Bean Properties

--- springListOfBeans/src/main/java/springListOfBeans/Jungle.java
+++ springListOfBeans/src/main/java/springListOfBeans/Jungle.java
@@ -6,18 +6,30 @@ import java.util.Map;
 public class Jungle {
 
 	private Map<String, String> foods = new HashMap<String, String>();
+	private Map<String, Animals> animals = new HashMap<String, Animals>();
 
 	public void setFoods(Map<String, String> foods) {
 		this.foods = foods;
+	}
+
+	public void setAnimals(Map<String, Animals> animals) {
+		this.animals = animals;
 	}
 
 	@Override
 	public String toString() {
 		
         StringBuilder sb = new StringBuilder();
+        sb.append("Property Maps" + "\n\n");
 		
 		for(Map.Entry<String, String> food: foods.entrySet()) {
 			sb.append(food.getKey() + ": " + food.getValue() + "\n");
+		}
+		
+		sb.append("\n" + "Arbitrary Maps as Bean Properties" + "\n\n");
+		
+		for(Map.Entry<String, Animals> animals: animals.entrySet()) {
+			sb.append(animals.getKey() + ": " + animals.getValue() + "\n");
 		}
 		
 		return sb.toString();
