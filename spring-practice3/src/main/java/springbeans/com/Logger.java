1c5794bc42dc40ce5846db73e9086bcc68fd470a
Slava Juravliov <prospect.sjp@gmail.com>
2019-10-23 14:57:11 +0100

    The "Autowired" Annotation

--- spring-practice3/src/main/java/springbeans/com/Logger.java
+++ spring-practice3/src/main/java/springbeans/com/Logger.java
@@ -1,15 +1,26 @@
 package springbeans.com;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
 	
+	//@Autowired
 	private ConsoleWriter consoleWriter;
+	//@Autowired
 	private FileWriter fileWriter;
-	
 
+	@Autowired
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
+
+	//@Autowired
 	public void setConsoleWriter(ConsoleWriter consoleWriter) {
 		this.consoleWriter = consoleWriter;
 	}
 	
+	//@Autowired
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
