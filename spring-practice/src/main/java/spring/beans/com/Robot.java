4f8b01d6ca413e81ab4ea393ab9caa0b843d8739
Slava Juravliov <prospect.sjp@gmail.com>
2019-11-05 14:10:06 +0000

    Working with Databases Using Property Files

--- spring-practice/src/main/java/spring/beans/com/Robot.java
+++ spring-practice/src/main/java/spring/beans/com/Robot.java
@@ -6,19 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
-
 @Component
 public class Robot {
 	
-	private int id = 0;
+	private String id = "Default robot";
 	private String speach = "Hello! ";
 	
-	@Autowired
-	public void setId(@Value("#{111}")int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
-	@Autowired
-	public void setSpeach(@Value("#{new java.util.Date().toString()}")String speach) {
+	public void setSpeach(String speach) {
 		this.speach = speach;
 	}
 	public void speak(){
