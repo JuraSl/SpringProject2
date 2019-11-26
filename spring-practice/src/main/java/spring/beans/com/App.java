763fb74c5bd64d0688cb1341258e40cf47dec23e
Slava Juravliov <prospect.sjp@gmail.com>
2019-11-26 11:19:10 +0000

    Database Exceptions

--- spring-practice/src/main/java/spring/beans/com/App.java
+++ spring-practice/src/main/java/spring/beans/com/App.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class App {
 
@@ -16,12 +18,18 @@ public class App {
 		
 		// getting beans from container passing an id "person"
 		OffersDao offersDao = (OffersDao)context.getBean("offersDao");
-		
-		List<Offers> list = offersDao.getOffers();
-		
-		for(Offers offer: list){
-			System.out.println(offer);
-		}
+		try{
+			List<Offers> list = offersDao.getOffers();		
+		    for(Offers offer: list){
+			   System.out.println(offer);
+			   }
+		    }
+		    catch(CannotGetJdbcConnectionException ex){
+		    	System.out.println(ex.getMessage());
+		    }catch(DataAccessException ex){
+		    	System.out.println(ex.getMessage());
+		    	System.out.println(ex.getClass());
+		    }
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
